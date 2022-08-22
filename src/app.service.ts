4a56b132d68963241a4ee1a4149a8bdef3458ca7
1589625130 <1589625130@qq.com>
2022-08-22 17:13:58 +0800

    update user

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { UserService } from './user/user.service'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!'
   }
 }
